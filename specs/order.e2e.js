feaa6effc825888151c1f74a6e99301f53916be3
Pavlina Vargova <pavlinavargovabrno@gmail.com>
2022-12-31 13:36:17 +0100

    The first version of homework

--- specs/order.e2e.js
+++ specs/order.e2e.js
@@ -10,8 +10,145 @@ import {
     endDate
 } from './fixtures.js'
 
-describe('Objednávka pro MŠ/ZŠ', () => {
+describe('Objednávka pro MŠ_ZŠ', () => {
 
-    // testy sem :)
+    it('Should open order page', () => {
+
+        browser.reloadSession();
+        browser.url('');
+        browser.pause(2000);
+        let forTeacherElement = $$('.nav-item.dropdown')[1];
+         forTeacherElement.click();
+         const orderForSchools = forTeacherElement.$$(".dropdown-item")[1];
+         expect(orderForSchools).isExisting;
+         expect(orderForSchools).isDisplayed;
+         expect(orderForSchools).isClickable;
+         orderForSchools.click();
+         browser.pause(3000);
+         const inputIco = $('#ico');
+         expect(inputIco).isExisting;
+         expect(inputIco).isDisplayed;
+         const titleOfPage = $('.card').$('.card-body').$('h3').getText();  
+         console.log('TITULNI STRANKA JE: ' + titleOfPage);
+         expect('Objednávka akce').toEqual(titleOfPage);
+    
+     });
+ 
+     it('Filling name and address from Ares', () =>{
+        browser.reloadSession();
+         browser.url('/objednavka/pridat');
+         
+         const inputIco = $('#ico');
+         inputIco.setValue(ICO);
+         browser.keys('Enter');
+         browser.pause(4000);
+ 
+         //asertace Aresu
+         const value = browser.execute((id) =>{
+             return document.getElementById(id).value;
+          }, 'client');
+          const valueOfAdress = browser.execute((id) =>{
+             return document.getElementById(id).value;
+          }, 'address');
+          console.log('HODNOTA JE: ' + value);
+          console.log('ADRESA JE: ' + valueOfAdress);
+          //browser.pause(3000);
+
+          expect(clientName).toEqual(value);
+          expect(address).toEqual(valueOfAdress); 
+     })
+
+     it('Fill the order Primestsky tabor', () => {
+ 
+         browser.reloadSession();
+         browser.url('/objednavka/pridat');
+         
+         const inputIco = $('#ico');
+         inputIco.setValue(ICO);
+         browser.keys('Enter');
+         browser.pause(4000);
+         const inputZastoupenaOsobou = $('#substitute');
+         inputZastoupenaOsobou.setValue(substituteName);
+         const inputContantName = $('#contact_name');
+         inputContantName.setValue(contactName);
+         const inputContactTel = $('#contact_tel');
+         inputContactTel.setValue(contactPhone);
+         const inputContactMail = $('#contact_mail');
+         inputContactMail.setValue(contactEmail);
+ 
+        // VYPLNIT POZADOVANY TERMIN
+        $('#start_date_1').setValue(startDate);
+        $('#end_date_1').setValue(endDate);
+        browser.pause(5000);
+ 
+        
+        const bottonPrimestskyTabor = $('#nav-home-tab');
+        bottonPrimestskyTabor.click();
+        $('#camp-date_part').selectByVisibleText('Odpolední');
+ 
+        const inputPocetDeti = $('#camp-students');
+        inputPocetDeti.setValue('17');
+        const inputVeVeku = $('#camp-age');
+        const inputAdults = $('#camp-adults');
+        inputVeVeku.setValue('6 - 9');
+        inputAdults.setValue('2');
+        browser.pause(3000);
+     
+      //klikni na objednani
+       $('.btn.btn-primary').click();
+        browser.pause(5000);
+        
+        const titleOfPage = $('.card').$('.card-body').$('h3').getText();
+        expect(titleOfPage).isExisting;
+        expect(inputIco).isNotExisting;
+        expect($('.card').$('.card-body').$('p')).isExisting;
+        console.log(titleOfPage);
+
+        //asertace, ze jde objednavka odeslat
+        expect('Děkujeme za objednávku').toEqual(titleOfPage); 
+     });
+
+     it('Do not sent order withou fillig inputs', () => {
+ 
+        browser.reloadSession();
+        browser.url('/objednavka/pridat');
+        
+        const inputIco = $('#ico');
+        inputIco.setValue(ICO);
+        browser.keys('Enter');
+        browser.pause(4000);
+        const inputZastoupenaOsobou = $('#substitute');
+        inputZastoupenaOsobou.setValue(substituteName);
+        const inputContantName = $('#contact_name');
+        inputContantName.setValue(contactName);
+        const inputContactTel = $('#contact_tel');
+        inputContactTel.setValue(contactPhone);
+        const inputContactMail = $('#contact_mail');
+        inputContactMail.setValue(contactEmail);
+
+       // VYPLNIT POZADOVANY TERMIN
+       $('#start_date_1').setValue(startDate);
+       $('#end_date_1').setValue(endDate);
+       browser.pause(5000);
+
+       
+       const bottonPrimestskyTabor = $('#nav-home-tab');
+       bottonPrimestskyTabor.click();
+       $('#camp-date_part').selectByVisibleText('Odpolední');
+
+       const inputPocetDeti = $('#camp-students');
+       inputPocetDeti.setValue('17');
+       browser.pause(3000);
+    
+     //klikni na objednani
+      $('.btn.btn-primary').click();
+       browser.pause(5000);
+     
+    //asertace, ze se neda objednavka odeslat
+       const titleOfPage = $('.card').$('.card-body').$('h3').getText();
+       console.log('TITUL STRANKY: ' + titleOfPage);
+       expect('Objednávka akce').toEqual(titleOfPage);   
+       expect(inputIco).isExisting;
+     });  
 
 });
--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,19 @@
+const fs = require('fs');
+const passedDirectory = 'screenshots/passed';
+const failedDirectory = 'screenshots/failed';
+
+function createIfNotExists(dir) {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+    }
+}
+
+function deleteFiles(dir) {
+    fs.rmdir(dir, { recursive: true }, err => {
+        if (err) console.log(err);
+    });
+}
+
 exports.config = {
     specs: [
         './specs/**/*.js'
@@ -29,7 +45,7 @@ exports.config = {
     }],
     logLevel: 'silent', // trace | debug | info | warn | error | silent
     bail: 0,
-    baseUrl: 'ADRESA TESTOVANE APLIKACE',
+    baseUrl: 'https://team8-2022brno.herokuapp.com',
     waitforTimeout: 10000,
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
@@ -41,5 +57,20 @@ exports.config = {
     mochaOpts: {
         ui: 'bdd',
         timeout: 60000
+    },
+
+    onPrepare: (config, capabilities) => {
+        deleteFiles("screenshots");
+    },
+
+    afterTest: (test, context, { error, result, duration, passed, retries }) => {
+        const screenshotName = (`${test.parent}__${test.title}.png`).replace(/ /g, '_');
+        if (passed === true) {
+            createIfNotExists(passedDirectory +"/passed");
+            browser.saveScreenshot( `${passedDirectory}/passed/${screenshotName}`);
+        } else {
+            createIfNotExists(failedDirectory+"/failed");
+            browser.saveScreenshot(`${failedDirectory}/failed/${screenshotName}`);
+        }
     }
 }
